88ca649f9b9553f85aec74dc61ece211aa80c9bf
Pavel Kizhlo <pavelkizh@gmail.com>
2022-09-22 22:05:13 +0300

    feat: add app template and common styles

--- aviasales/src/app/app.module.ts
+++ aviasales/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { SortComponent } from './components/sort/sort.component';
+import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    SortComponent,
+    ButtonComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
